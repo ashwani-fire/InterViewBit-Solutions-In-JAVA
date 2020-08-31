86e4d244691975f1a22a51e3803d00d27e76a614
firebee-ai <64791632+firebee-ai@users.noreply.github.com>
2020-09-01 00:04:11 +0530

    Update wave Army.java

--- Array/wave Army.java
+++ Array/wave Army.java
@@ -1,1 +1,19 @@
+/*Sort The Array And Swap The Adjacent Elements */
+public class Solution {
+    public void swap(int A,int B){
+        int temp=0;
+        temp = B;
+        B=A;
+        A=temp;
+    }
+    public ArrayList<Integer> wave(ArrayList<Integer> A) {
+        Collections.sort(A);
+        for(int i=0;i<A.size()-1;i=i+2){
+            int tmp = A.get(i+1);
+            A.get(i+1) =A.get(i);
+            A.get(i) = tmp;
+        }
+        return A;
+    }
+}
 
