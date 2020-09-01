7dd642385d3042cfd0d4eb9f04e7bdf4d09f31a6
firebee-ai <64791632+firebee-ai@users.noreply.github.com>
2020-09-01 13:41:25 +0530

    Update Excel_col_2.java

--- Math/Excel_col_2.java
+++ Math/Excel_col_2.java
@@ -1,1 +1,24 @@
 
+public class Solution {
+    public String convertToTitle(int A) {
+        		StringBuilder sb = new StringBuilder();
+
+        int i=0;
+        while(A>0){
+         if (A%26 == 0) {
+                sb.append("Z");
+                A = A/26 - 1;
+            }
+else{
+            int k=A%26+'A'-1;
+            char c = (char)k;
+           
+            sb.append(c);
+            A=A/26;
+}
+        }
+        sb=sb.reverse();
+            return sb.toString();
+ 
+    }
+}
