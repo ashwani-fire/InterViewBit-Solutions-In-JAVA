5e9931751392ed203a4c30010280366b841460f5
firebee-ai <64791632+firebee-ai@users.noreply.github.com>
2020-09-01 13:11:13 +0530

    Update Excel NUmber Count.java

--- Math/Excel NUmber Count.java
+++ Math/Excel NUmber Count.java
@@ -1,1 +1,11 @@
-
+/*Easiest Solution */
+public class Solution {
+    public int titleToNumber(String A) {
+    int sum=0;
+    for(int i=0;i<A.length();i++){
+        sum = sum*26;
+        sum = sum + A.charAt(i)-'A'+1;
+    }
+        return sum;
+    }
+}
